d8811fb43e501e83193727678cd22bdc45b473d0
Guillermo <folez97@gmail.com>
2019-10-16 18:02:50 +0000

    commit

--- goodreads.js
+++ goodreads.js
@@ -1,7 +1,6 @@
 const goodreads = require('goodreads-api-node');
 const myCredentials = {
-  key: '6YHn27A8YWetke58jdgQHw',
-  secret: 'AwvwbzYia7Ub2Qdl3xAwrakcWZnCxRscaqvEtHvQWU'
+
 };
 const gr = goodreads(myCredentials);
 module.exports = {gr}
