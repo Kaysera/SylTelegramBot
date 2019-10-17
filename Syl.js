87b19435e2168a415424cffc5f1f276edc3abbe1
Guillermo <folez97@gmail.com>
2019-10-17 14:51:40 +0000

    Added more functions and solved some ES6 conflicts

--- Syl.js
+++ Syl.js
@@ -3,11 +3,14 @@ const {GoodreadsTokenModel} = require('./Models/GoodreadsToken.js')
 const {gr} = require('./goodreads.js')
 const generadorInsultos = require('./generadorInsultos')
 
-const token = process.env.BOT_TOKEN;
+const botToken = process.env.BOT_TOKEN;
+var token
+var readingShelf = 'currently-reading'
+var selectedBook
 
-export default class Syl {
+class Syl {
   constructor() {
-    let bot = new TelegramBot(token, {polling: true});
+    let bot = new TelegramBot(botToken, {polling: true});
     bot = this.configure(bot)
 
     this.bot = bot
@@ -15,18 +18,24 @@ export default class Syl {
   }
   configure(bot) {
     bot.on('polling_error', (err) => console.error(err))
+    bot.on('callback_query', (callbackQuery) => this.onCallbackQuery(callbackQuery))
+
     bot = this.addGREndpoints(bot);
     bot = this.addDebugEndpoints(bot);
     return bot;
   }
   addGREndpoints(bot) {
-    bot.onText(/\/login/, this.loginGR)
-    bot.onText(/\/getBook/,this.getBookGR)
+    bot.onText(/\/login/, this.loginGR.bind(this))
+    bot.onText(/\/saluda/, this.saluda.bind(this))
+    bot.onText(/\/updatebookprogress/, this.updateBookProgressGR.bind(this))
+    bot.onText(/\/setpercent/, this.setPercentGR.bind(this))
+    bot.onText(/\/setpage/, this.setPageGR.bind(this))
+
     return bot;
   }
   loginGR(msg) {
     var query = {chatID: msg.chat.id}
-    GoodreadsTokenModel.findOne(query, function(err, result) {
+    GoodreadsTokenModel.findOne(query, (err, result) => {
       if (err) throw err;
       if(result == null){
         this.sendMessage(msg.chat.id, `Click on the following link: ${address}/login?chatid=${msg.chat.id}`)
@@ -35,13 +44,95 @@ export default class Syl {
       }
     })
   }
-  getBookGR(msg) {
-    gr.getBooksByAuthor('175417')
-    .then(res => {
-      console.log(res.name)
-      bot.sendMessage(msg.chat.id, res.name)
-    });
+
+
+  saluda(msg) {
+    this.sendMessage(msg.chat.id, 'Hi, my name is Syl. How can I help you?')
   }
+
+  updateBookProgressGR(msg) {
+      var query = {chatID: msg.chat.id}
+      var keyboard = []
+      this.checkLoginGR(query, (result) => {
+        gr.initOAuth();
+          token = { ACCESS_TOKEN: result.accessToken, ACCESS_TOKEN_SECRET: result.accessSecret }
+          gr.setAccessToken(token).then(() => {
+            gr.getBooksOnUserShelf(result.goodreadsID, readingShelf).then(res => {
+              var myBooks = res.books.book;
+              for (var element in myBooks) {
+                var book = [{text: `${myBooks[element].title} - ${myBooks[element].authors.author.name}`, callback_data: `updatebookprogress - ${myBooks[element].id._} - ${element}`}]
+                keyboard.push(book)
+              }
+              this.sendMessage(msg.chat.id, `Select the book you want to update: `, {reply_markup: {inline_keyboard: keyboard}})
+            })
+          })
+      })
+  }
+
+  checkLoginGR(query, callback) {
+    GoodreadsTokenModel.findOne(query, (err, result)  => {
+      if (err) throw err;
+      if(result == null){
+        this.sendMessage(msg.chat.id, `You are not logged in. Please use the command /login`)
+      }else{
+        callback(result)
+      }
+    })
+  }
+
+  setPercentGR(msg, match) {
+    if (selectedBook === ''){
+      this.sendMessage(msg.chat.id, `No book selected`)
+      return
+    }
+
+    var query = {chatID: msg.chat.id}
+    var percent = match.input.split(' ')[1]
+    if (percent < 0 || percent > 100 || isNaN(percent)) {
+      this.sendMessage(msg.chat.id, `Percentage not valid`)
+      return
+    }
+
+    this.checkLoginGR(query, (result) => {
+        gr.initOAuth();
+        token = { ACCESS_TOKEN: result.accessToken, ACCESS_TOKEN_SECRET: result.accessSecret }
+        gr.setAccessToken(token).then(() => {
+          gr.setReadPercent(selectedBook, percent).then(result => {
+            console.log(result)
+            this.sendMessage(msg.chat.id, `Book progress updated successfully!`)
+            selectedBook = ''
+          })
+        })
+      })
+  }
+
+  setPageGR(msg, match) {
+    if (selectedBook === ''){
+      this.sendMessage(msg.chat.id, `No book selected`)
+      return
+    }
+
+    var query = {chatID: msg.chat.id}
+    var page = match.input.split(' ')[1]
+    // TODO: Comprobar si la página está dentro del rango del libro
+    // if (percent < 0 || percent > 100 || isNaN(percent)) {
+    //   this.sendMessage(msg.chat.id, `Percentage not valid`)
+    //   return
+    // }
+
+    this.checkLoginGR(query, (result) => {
+        gr.initOAuth();
+        token = { ACCESS_TOKEN: result.accessToken, ACCESS_TOKEN_SECRET: result.accessSecret }
+        gr.setAccessToken(token).then(() => {
+          gr.setReadPage(selectedBook, page).then(result => {
+            console.log(result)
+            this.sendMessage(msg.chat.id, `Book progress updated successfully!`)
+            selectedBook = ''
+          })
+        })
+      })
+    }
+
   addDebugEndpoints(bot) {
     bot.onText(/\/whoami/, this.getUserId)
     return bot;
@@ -54,8 +145,22 @@ export default class Syl {
         bot.sendMessage(msg.chat.id, JSON.stringify(resp))
       })
   }
-  sendMessage(chatId, message) {
+  sendMessage(chatId, message, opts) {
     const insulto = generadorInsultos();
-    this.bot.sendMessage(chatId, `${message}, ${insulto}`)
+    const endChars = '?!,:'
+    var msg = (endChars.includes(message.trim().slice(-1))) ? `${message.trim().slice(0,-1)}, ${insulto}${message.trim().slice(-1)}` : `${message.trim()}, ${insulto}`
+    this.bot.sendMessage(chatId, msg, opts)
   }
+
+  onCallbackQuery(callbackQuery) {
+    const action = callbackQuery.data;
+    const msg = callbackQuery.message;
+    if (action.includes('updatebookprogress')) {
+      selectedBook = action.split('-')[1].trim()
+      this.sendMessage(msg.chat.id, `You have selected *${msg.reply_markup.inline_keyboard[action.split('-')[2].trim()][0].text}*\n\nTo set the progress, use /setpage or /setpercent followed by the new progress`, {parse_mode: 'Markdown'});
+    }
+  }
+
 }
+
+module.exports = Syl
--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const {GoodreadsTokenModel} = require('./Models/GoodreadsToken.js')
 
 const app = express();
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
-
 const SylBot = new Syl();
 
 app.get('/', function (req, res) {
@@ -51,115 +50,6 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-
-
-bot.onText(/\/saluda/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, 'Hi, my name is Syl. How can I help you?')
-})
-
-bot.onText(/\/setpercent/, (msg, match) => {
-
-  if (selectedBook === ''){
-    bot.sendMessage(msg.chat.id, `No book selected`)
-    return
-  }
-
-  var query = {chatID: msg.chat.id}
-  var percent = match.input.split(' ')[1]
-  if (percent < 0 || percent > 100 || isNaN(percent)) {
-    bot.sendMessage(msg.chat.id, `Percentage not valid`)
-    return
-  }
-
-  GoodreadsTokenModel.findOne(query, function(err, result) {
-    if (err) throw err;
-    if(result == null){
-      bot.sendMessage(msg.chat.id, `You are not logged in. Please use the command /login`)
-    }else{
-      gr.initOAuth();
-      token = { ACCESS_TOKEN: result.accessToken, ACCESS_TOKEN_SECRET: result.accessSecret }
-      gr.setAccessToken(token).then(() => {
-        gr.setReadPercent(selectedBook, percent).then(result => {
-          console.log(result)
-          bot.sendMessage(msg.chat.id, `Book progress updated successfully!`)
-          selectedBook = ''
-        })
-      })
-    }
-  })
-
-})
-
-bot.onText(/\/setpage/, (msg, match) => {
-
-  if (selectedBook === ''){
-    bot.sendMessage(msg.chat.id, `No book selected`)
-    return
-  }
-
-  var query = {chatID: msg.chat.id}
-  var page = match.input.split(' ')[1]
-  // TODO: Comprobar si la página está dentro del rango del libro
-  // if (percent < 0 || percent > 100 || isNaN(percent)) {
-  //   bot.sendMessage(msg.chat.id, `Percentage not valid`)
-  //   return
-  // }
-
-  GoodreadsTokenModel.findOne(query, function(err, result) {
-    if (err) throw err;
-    if(result == null){
-      bot.sendMessage(msg.chat.id, `You are not logged in. Please use the command /login`)
-    }else{
-      gr.initOAuth();
-      token = { ACCESS_TOKEN: result.accessToken, ACCESS_TOKEN_SECRET: result.accessSecret }
-      gr.setAccessToken(token).then(() => {
-        gr.setReadPage(selectedBook, page).then(result => {
-          console.log(result)
-          bot.sendMessage(msg.chat.id, `Book progress updated successfully!`)
-          selectedBook = ''
-        })
-      })
-    }
-  })
-
-})
-
-bot.onText(/\/updatebookprogress/, (msg, match) => {
-
-  var query = {chatID: msg.chat.id}
-  var keyboard = []
-  GoodreadsTokenModel.findOne(query, function(err, result) {
-    if (err) throw err;
-    if(result == null){
-      bot.sendMessage(msg.chat.id, `You are not logged in. Please use the command /login`)
-    }else{
-      gr.initOAuth();
-      token = { ACCESS_TOKEN: result.accessToken, ACCESS_TOKEN_SECRET: result.accessSecret }
-      gr.setAccessToken(token).then(() => {
-        gr.getBooksOnUserShelf(result.goodreadsID, readingShelf).then(res => {
-          var myBooks = res.books.book;
-          for (var element in myBooks) {
-            var book = [{text: `${myBooks[element].title} - ${myBooks[element].authors.author.name}`, callback_data: `updatebookprogress - ${myBooks[element].id._} - ${element}`}]
-            keyboard.push(book)
-          }
-          bot.sendMessage(msg.chat.id, `Select the book you want to update: `, {reply_markup: {inline_keyboard: keyboard}})
-        })
-      })
-    }
-  })
-
-})
-
-
-// Handle callback queries
-bot.on('callback_query', function onCallbackQuery(callbackQuery) {
-  const action = callbackQuery.data;
-  const msg = callbackQuery.message;
-  if (action.includes('updatebookprogress')) {
-    selectedBook = action.split('-')[1].trim()
-    bot.sendMessage(msg.chat.id, `You have selected *${msg.reply_markup.inline_keyboard[action.split('-')[2].trim()][0].text}*\n\nTo set the progress, use /setpage or /setpercent followed by the new progress`, {parse_mode: 'Markdown'});
-  }
-});
 
 
 
